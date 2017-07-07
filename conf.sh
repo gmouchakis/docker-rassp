1ffdd258b0e6575d0a9dc5937e570a7be7e3da80
Giannis Mouchakis <gmouchakis@iit.demokritos.gr>
2017-07-07 16:14:29 +0300

    group-seeds renamed to group-proxies

--- conf.sh
+++ conf.sh
@@ -6,5 +6,5 @@ export DOCKER_IP=`hostname -i`
 sed -i "s#{?HOSTNAME}#$HOSTNAME#g" $CONF
 sed -i "s#{?SEED_NODE}#$SEED_NODE#g" $CONF
 sed -i "s#{?PEERS}#$PEERS#g" $CONF
-sed -i "s#{?GROUP_SEEDS}#$GROUP_SEEDS#g" $CONF
+sed -i "s#{?GROUP_PROXIES}#$GROUP_PROXIES#g" $CONF
 sed -i "s#{?DOCKER_IP}#$DOCKER_IP#g" $CONF
